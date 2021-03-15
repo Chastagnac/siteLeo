76b313eda31f4778248cb40b3cc5ffa8e92acacb
leo <leochastagnac@gmail.com>
2021-03-15 12:47:43 +0100

    reponsive project debut

--- page/project.php
+++ page/project.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../css/project.css" />
-    <link rel="stylesheet" href="../css/responsive.css" />
+    <link rel="stylesheet" href="../css/reponsiveProject.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
     <script src="https://kit.fontawesome.com/d7d3f46c80.js" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/scrollreveal"></script>
@@ -17,11 +17,11 @@
     <section id="sec-1">
         <div class="bandeau">
             <h3>Projets</h3>
-            <p>Vous retrouverer ici des projets d'étudiant mais</p>
-            <p>également des projets personnels.</p>
+            <p>Vous retrouverez ici des projets d'étudiant mais
+            également des projets personnels.</p>
         </div>
     </section>
-    <section id="sec-2">
+    <!-- <section id="sec-2">
         <h4>Projets de BTS SIO</h4>
         <div class="container">
             <img src="../assets/images/gsb.jpg" alt="Grapefruit slice atop a pile of other slices">
@@ -33,7 +33,7 @@
 
             </div>
         </div>
-    </section>
+    </section> -->
 
 
 
