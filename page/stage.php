e1fed3ecbe4702cebd128c902920bd4c9dd875bf
leochastagnac <leochastagnac@gmail.com>
2021-04-09 13:50:48 +0200

    stage premice

--- page/stage.php
+++ page/stage.php
@@ -6,9 +6,61 @@
     <title>Chastagnac Léo</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="../css/stage.css" />
     <link rel="stylesheet" href="../css/responsive.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
     <script src="https://kit.fontawesome.com/d7d3f46c80.js" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/scrollreveal"></script>
 </head>
+
+<body>
+    <section id="sec-1">
+        <h2>Stage de BTS SIO.</h2>
+        <div class="container">
+            <p> Le BTS Services Informatiques aux Organisations offre la possibilité d'effectuer 2 stages en entreprise durant le cursus. Néanmoins, suite au Covid, notre stage de première année s'est vu annulé
+                Pour pallier à ce manque, nous avons organisé un groupe de travail pour créer un jeu 2d : Alban's Adventure.
+            </p>
+        </div>
+        <img src="../assets/images/unityicon.png" alt="image du logo unity">
+    </section>
+    <section id="sec-2">
+        <div class="gallery">
+            <a target="_blank" href="../assets/images/imgunity/1.PNG">
+                <img src="../assets/images/imgunity/1.PNG" alt="arme png" width="600" height="400">
+            </a>
+            <div class="desc">Armes de jeu</div>
+        </div>
+
+        <div class="gallery">
+            <a target="_blank" href="../assets/images/imgunity/2.PNG">
+                <img src="../assets/images/imgunity/2.PNG" alt="armures png" width="600" height="400">
+            </a>
+            <div class="desc">Armures du jeu</div>
+        </div>
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="../assets/images/imgunity/3.PNG" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc">Add a description of the image here</div>
+        </div>
+
+        <div class="gallery">
+            <a target="_blank" href="img_forest.jpg">
+                <img src="../assets/images/imgunity/1.PNG" alt="Forest" width="600" height="400">
+            </a>
+            <div class="desc">Add a description of the image here</div>
+        </div>
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="../assets/images/imgunity/3.PNG" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc">Add a description of the image here</div>
+        </div>
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="../assets/images/imgunity/3.PNG" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc">Add a description of the image here</div>
+        </div>
+    </section>
+</body>
