068912cc8ff3718db38463cd8d7c29a19dcda572
Léo Chastagnac <65346336+Chastagnac@users.noreply.github.com>
2021-04-10 20:49:53 +0200

    unitypage + retirer unity 1

--- page/stage.php
+++ page/stage.php
@@ -24,43 +24,57 @@
         <img src="../assets/images/unityicon.png" alt="image du logo unity">
     </section>
     <section id="sec-2">
-        <div class="gallery">
-            <a target="_blank" href="../assets/images/imgunity/1.PNG">
-                <img src="../assets/images/imgunity/1.PNG" alt="arme png" width="600" height="400">
-            </a>
-            <div class="desc">Armes de jeu</div>
-        </div>
+        <div class="container">
+            <div class="gallery">
+                <a target="_blank" href="../assets/images/imgunity/1.PNG">
+                    <img src="../assets/images/imgunity/1.PNG" alt="arme png" width="600" height="400">
+                </a>
+                <div class="desc">Armes de jeu</div>
+            </div>
 
-        <div class="gallery">
-            <a target="_blank" href="../assets/images/imgunity/2.PNG">
-                <img src="../assets/images/imgunity/2.PNG" alt="armures png" width="600" height="400">
-            </a>
-            <div class="desc">Armures du jeu</div>
-        </div>
-        <div class="gallery">
-            <a target="_blank" href="img_5terre.jpg">
-                <img src="../assets/images/imgunity/3.PNG" alt="Cinque Terre" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
+            <div class="gallery">
+                <a target="_blank" href="../assets/images/imgunity/2.PNG">
+                    <img src="../assets/images/imgunity/2.PNG" alt="armures png" width="600" height="400">
+                </a>
+                <div class="desc">Armures du jeu</div>
+            </div>
+            <div class="gallery">
+                <a target="_blank" href="../assets/images/imgunity/3.PNG">
+                    <img src="../assets/images/imgunity/3.PNG" alt="img jeu" width="600" height="400">
+                </a>
+                <div class="desc">Add a description of the image here</div>
+            </div>
 
-        <div class="gallery">
-            <a target="_blank" href="img_forest.jpg">
-                <img src="../assets/images/imgunity/1.PNG" alt="Forest" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-        <div class="gallery">
-            <a target="_blank" href="img_5terre.jpg">
-                <img src="../assets/images/imgunity/3.PNG" alt="Cinque Terre" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-        <div class="gallery">
-            <a target="_blank" href="img_5terre.jpg">
-                <img src="../assets/images/imgunity/3.PNG" alt="Cinque Terre" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
+            <div class="gallery">
+                <a target="_blank" href="../assets/images/imgunity/12.PNG">
+                    <img src="../assets/images/imgunity/12.PNG" alt="img jeu" width="600" height="400">
+                </a>
+                <div class="desc">Add a description of the image here</div>
+            </div>
+            <div class="gallery">
+                <a target="_blank" href="../assets/images/imgunity/13.PNG">
+                    <img src="../assets/images/imgunity/13.PNG" alt="img jeu" width="600" height="400">
+                </a>
+                <div class="desc">Add a description of the image here</div>
+            </div>
+            <div class="gallery">
+                <a target="_blank" href="../assets/images/imgunity/14.PNG">
+                    <img src="../assets/images/imgunity/14.PNG" alt="img jeu" width="600" height="400">
+                </a>
+                <div class="desc">Add a description of the image here</div>
+            </div>
         </div>
     </section>
+    <div id="sec-3">
+        <div class="container">
+            <img src="../assets/images/imgunity/unity.png" alt="img de l'interace de unity">
+            <div class="texte">
+                <h2>Unity ?</h2>
+                <p>Unity 3D est un moteur 3D. C’est un logiciel permettant de créer des jeux vidéo 2D ou 3D. Il s’apparente à un logiciel de modélisation 3D comme Blender : il intègre donc une partie graphique et une partie programmation pour la création des projets.
+                    <br>Ce moteur à la fois un outil orienté graphisme, avec une interface agréable à prendre en main et orienté programmation avec une API très complète.
+                    Unity3D est un moteur puissant mais accessible. Afin d’accéder à toutes les potentialités qu’il offre et de créer un contenu réellement interactif, la programmation s’impose : sous Unity, tout est « scriptable », notamment grâce au langage C#.
+                    <br>En résumé, Unity permet de créer tout type de contenu, allant de tous les genres de jeux vidéo à l’application pour tablette/smartphone en passant par le site internet en HTML5.</p>
+            </div>
+        </div>
+    </div>
 </body>
