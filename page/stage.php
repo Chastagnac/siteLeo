605acd0b8042dec54d7d5df7253954d59f797135
Léo Chastagnac <65346336+Chastagnac@users.noreply.github.com>
2021-04-12 00:14:25 +0200

    stage evoliz + reponsive

--- page/stage.php
+++ page/stage.php
@@ -7,15 +7,19 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../css/stage.css" />
-    <link rel="stylesheet" href="../css/responsive.css" />
+    <link rel="stylesheet" href="../css/reponsivestage.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
     <script src="https://kit.fontawesome.com/d7d3f46c80.js" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/scrollreveal"></script>
 </head>
 
 <body>
+    <a href="../index.php">
+        <i class="fas fa-home"></i>
+    </a>
     <section id="sec-1">
-        <h2>Stage de BTS SIO.</h2>
+        <h2>Stage de BTS SIO</h2>
+        <h1>Unity</h1>
         <div class="container">
             <p> Le BTS Services Informatiques aux Organisations offre la possibilité d'effectuer 2 stages en entreprise durant le cursus. Néanmoins, suite au Covid, notre stage de première année s'est vu annulé
                 Pour pallier à ce manque, nous avons organisé un groupe de travail pour créer un jeu 2d : Alban's Adventure.
@@ -42,26 +46,26 @@
                 <a target="_blank" href="../assets/images/imgunity/3.PNG">
                     <img src="../assets/images/imgunity/3.PNG" alt="img jeu" width="600" height="400">
                 </a>
-                <div class="desc">Add a description of the image here</div>
+                <div class="desc">Décors & joueur</div>
             </div>
 
             <div class="gallery">
                 <a target="_blank" href="../assets/images/imgunity/12.PNG">
                     <img src="../assets/images/imgunity/12.PNG" alt="img jeu" width="600" height="400">
                 </a>
-                <div class="desc">Add a description of the image here</div>
+                <div class="desc">Shop, monnaie, armure et santé</div>
             </div>
             <div class="gallery">
                 <a target="_blank" href="../assets/images/imgunity/13.PNG">
                     <img src="../assets/images/imgunity/13.PNG" alt="img jeu" width="600" height="400">
                 </a>
-                <div class="desc">Add a description of the image here</div>
+                <div class="desc">Terrain sous terre</div>
             </div>
             <div class="gallery">
                 <a target="_blank" href="../assets/images/imgunity/14.PNG">
                     <img src="../assets/images/imgunity/14.PNG" alt="img jeu" width="600" height="400">
                 </a>
-                <div class="desc">Add a description of the image here</div>
+                <div class="desc">Terrain sous terre</div>
             </div>
         </div>
     </section>
@@ -73,8 +77,35 @@
                 <p>Unity 3D est un moteur 3D. C’est un logiciel permettant de créer des jeux vidéo 2D ou 3D. Il s’apparente à un logiciel de modélisation 3D comme Blender : il intègre donc une partie graphique et une partie programmation pour la création des projets.
                     <br>Ce moteur à la fois un outil orienté graphisme, avec une interface agréable à prendre en main et orienté programmation avec une API très complète.
                     Unity3D est un moteur puissant mais accessible. Afin d’accéder à toutes les potentialités qu’il offre et de créer un contenu réellement interactif, la programmation s’impose : sous Unity, tout est « scriptable », notamment grâce au langage C#.
-                    <br>En résumé, Unity permet de créer tout type de contenu, allant de tous les genres de jeux vidéo à l’application pour tablette/smartphone en passant par le site internet en HTML5.</p>
+                    <br>En résumé, Unity permet de créer tout type de contenu, allant de tous les genres de jeux vidéo à l’application pour tablette/smartphone en passant par le site internet en HTML5.
+                </p>
             </div>
         </div>
     </div>
+    <section id="sec-4">
+        <hr>
+        <br>
+        <h1>Evoliz</h1>
+        <div class="container">
+            <p> Le BTS Services Informatiques aux Organisations offre la possibilité d'effectuer 2 stages en entreprise durant le cursus. Néanmoins, suite au Covid, notre stage de première année s'est vu annulé
+                Pour pallier à ce manque, nous avons organisé un groupe de travail pour créer un jeu 2d : Alban's Adventure.
+            </p>
+        </div>
+        <img src="../assets/images/imgevoliz/logo.jpg" alt="image du logo evoliz">
+        <div class="method">
+            <h2>Méthodes de travail : </h2>
+            <ul>
+                <li>PHP</li>
+                <li>PHP</li>
+                <li>PHP</li>
+                <li>PHP</li>
+            </ul>
+        </div>
+        <div class="photo">
+            <img src="../assets/images/imgevoliz/logo.jpg" alt="image du logo evoliz">
+            <img src="../assets/images/imgevoliz/logo.jpg" alt="image du logo evoliz">
+            <img src="../assets/images/imgevoliz/logo.jpg" alt="image du logo evoliz">
+        </div>
+
+    </section>
 </body>
